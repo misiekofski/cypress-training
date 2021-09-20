2c6f8f04c647b39cbf5554699804fa603a505bb7
Michal Dobrzycki <dobrzycki@gmail.com>
2021-09-20 21:30:36 +0200

    Finished 3 tasks for POP

--- cypress/integration/day2/1-page-object-pattern/3-add-testdata-provider.spec.js
+++ cypress/integration/day2/1-page-object-pattern/3-add-testdata-provider.spec.js
@@ -1,3 +1,29 @@
 /// <reference types="cypress" />
 
 // Task #3
+
+
+/// <reference types="cypress" />
+import { LoginPage } from "../../../pages/LoginPage/LoginPage";
+const loginPage = new LoginPage();
+// Task #3
+
+describe('multiple users test', () => {
+  // We cannot load JSON file using "cy.fixture"
+  // because it means the test is already running.
+  // Same with using "before" hook - new tests cannot be created from "before" hook.
+  // Instead we need to load JSON file using "require" at the start time  
+  const users = require('../../../fixtures/userlogins');
+
+  users.forEach((user) => {
+      it(`logs user: ${user.email}`, () => {
+        cy.visit('https://portal.vizlib.com/auth/login');
+        cy.get('input[formcontrolname="email"]').type(user.email);
+        cy.get('.mat-button-wrapper').click();
+        cy.get('input[formcontrolname="password"]').type(user.password);
+        cy.get('.mat-button-wrapper').click();
+
+        cy.get('span.username').should('have.text', 'Michal Dobrzycki');
+      })
+  })
+})
